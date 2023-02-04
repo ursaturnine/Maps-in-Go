374069c2af477763be42b7e4216b68f27c6dcd52
Tyrah <tyrah96@gmail.com>
2023-02-04 15:10:49 -0800

    two more ways to declare a map and an empty map. Also delete a key

--- main.go
+++ main.go
@@ -3,11 +3,26 @@ package main
 import "fmt"
 
 // declaring a map
+// [<keys of type ...>]<values of type...>
+// access values in map with <variable_name>[<key_name>]
 func main() {
-	colors := map[string]string{
-		"red":   "#ff000",
-		"green": "#gr33n",
-	}
+	//option 2 - creates empty amp
+	// var colors map[string]string
+
+	//option 3 -creates empty map
+	colors := make(map[int]string)
+
+	// option 1
+	// colors := map[string]string{
+	// 	"red":   "#ff000",
+	// 	"green": "#gr33n",
+	// }
+
+	colors[10] = "#ffffff"
+
+	// delete a key
+	// delete(<variable_name>, <key_name>)
+	delete(colors, 10)
 
 	fmt.Println(colors)
 }
